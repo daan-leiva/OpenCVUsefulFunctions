5081ef00a84cf594c4c8f7f739170f3aef8c9c0c
Daan Leiva <daan.leiva@gmail.com>
2016-02-10 23:48:23 -0800

    Added filtering functions

--- OpenCVUSefulPrograms/Source.cpp
+++ OpenCVUSefulPrograms/Source.cpp
@@ -13,32 +13,54 @@ int display_image();
 int display_video_basic();
 int display_video_trackbar();
 void onTrackbarSlide(int, void*);
+int image_transform_gausian_blur();
+int image_transform_blur();
+int image_transform_median_blur();
+int image_transform_bilateral_filter();
+
+// global variables
+const char* image_file_path = R"(C:\Users\ResearchBeast\Documents\Visual Studio 2015\Projects\OpenCVUSefulPrograms\OpenCVUSefulPrograms\Debug\SamplePipeCrack.jpg)";
+const char* video_file_path = R"(C:\Users\ResearchBeast\Documents\E16-SMH-041-E16-SMH-039_012215_1372_1.MPG)";
 
 int main(int argc, char** argv) {
 	int failed; // will store value of program to check if it successed
 	int choice; // user choice to run program
+	while (1)
+	{
+		display_menu();
+		cin >> choice;
 
-	display_menu();
-	cin >> choice;
+		switch (choice) {
+		case 1:
+			failed = display_image();
+			break;
+		case 2:
+			failed = display_video_basic();
+			break;
+		case 3:
+			failed = display_video_trackbar();
+			break;
+		case 4:
+			failed = image_transform_gausian_blur();
+			break;
+		case 5:
+			failed = image_transform_blur();
+			break;
+		case 6:
+			failed = image_transform_median_blur();
+			break;
+		case 7:
+			failed = image_transform_bilateral_filter();
+			break;
+		default:
+			break;
+		}
 
-	switch (choice) {
-	case 1:
-		failed = display_image();
-		break;
-	case 2:
-		failed = display_video_basic();
-		break;
-	case 3:
-		failed = display_video_trackbar();
-		break;
-	default:
-		break;
+		if (failed)
+			cout << "Program failed\n";
+		else
+			cout << "Program executed succesfully!\n";
 	}
-
-	if (failed)
-		cout << "Program failed\n";
-	else
-		cout << "Program executed succesfully!\n";
 }
 
 void display_menu() {
@@ -46,6 +68,10 @@ void display_menu() {
 	cout << "1. Display image\n";
 	cout << "2. Display video - basic\n";
 	cout << "3. Display video - trackbar\n";
+	cout << "4. Image transform - Gausian Blur\n";
+	cout << "5. Image transform - Blur\n";
+	cout << "6. Image transform - Median Blur\n";
+	cout << "7. Image transform - Bilateral Filter -- ((Still fixing))\n";
 }
 
 //
@@ -55,7 +81,6 @@ void display_menu() {
 // simple image display
 // no processing
 int display_image() {
-	const char* image_file_path = R"(C:\Users\ResearchBeast\Documents\Visual Studio 2015\Projects\OpenCVUSefulPrograms\OpenCVUSefulPrograms\Debug\SamplePipeCrack.jpg)";
 	Mat img = imread(image_file_path); // standard opencv object to hold matrices
 
 	// check if img loaded
@@ -88,7 +113,7 @@ int display_video_basic() {
 	namedWindow(window_name, WINDOW_AUTOSIZE);
 	// VideoCaptures contains information about the video about to be displayed
 	VideoCapture cap;
-	cap.open(R"(C:\Users\ResearchBeast\Documents\E16-SMH-041-E16-SMH-039_012215_1372_1.MPG)");
+	cap.open(video_file_path);
 	// check that video opened succesfully
 	if (!cap.isOpened())
 		return -1;
@@ -118,57 +143,49 @@ int display_video_basic() {
 // variables for trackbar program
 // keeps track globally of the trackbar position
 int g_slider_position = 0;
+
 // controls whether the video is runnning or not
 // g_run > 0 : number of frames displayed before stopping
 // g_run < 0 : systems runs in continuous video mode
 // g_run == 0 : video is not running
 int g_run = 1;
+
 // 
 int g_dontset = 0;
+
 // make this a global variable so that display_vide_trackbar
 // and onTrackbarSlide can access it
 VideoCapture g_cap;
 
 // video with track bar and frame step
 int display_video_trackbar() {
-	char* window_name = "Trackbar Example";
-	namedWindow(window_name, WINDOW_AUTOSIZE);
-	g_cap.open(R"(C:\Users\ResearchBeast\Documents\E16-SMH-041-E16-SMH-039_012215_1372_1.MPG)");
-	if (!g_cap.isOpened())
-		return -1;
-	// total number of frames in the video
+	cv::namedWindow("Example2_4", cv::WINDOW_AUTOSIZE);
+	g_cap.open(video_file_path);
 	int frames = (int)g_cap.get(cv::CAP_PROP_FRAME_COUNT);
-	// width of video
 	int tmpw = (int)g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
-	// height of video
 	int tmph = (int)g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 	cout << "Video has " << frames << " frames of dimensions("
 		<< tmpw << ", " << tmph << ")." << endl;
-	// create trackbar with label
-	//
-	createTrackbar("Position", window_name, &g_slider_position, frames, onTrackbarSlide);
-	Mat frame;
+	cv::createTrackbar("Position", "Example2_4", &g_slider_position, frames,
+		onTrackbarSlide);
+	cv::Mat frame;
 	while (1) {
 		if (g_run != 0) {
-			g_cap >> frame;
-			if (!frame.data)
-				break;
+			g_cap >> frame; if (!frame.data) break;
 			int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES);
 			g_dontset = 1;
-			cv::setTrackbarPos("Position", window_name, current_pos);
-			cv::imshow(window_name, frame);
+			cv::setTrackbarPos("Position", "Example2_4", current_pos);
+			cv::imshow("Example2_4", frame);
 			g_run -= 1;
 		}
-		char c = (char)cv::waitKey(1);
+		char c = (char)cv::waitKey(10);
 		if (c == 's') // single step
 		{
-			g_run = 1;
-			cout << "Single step, run = " << g_run << endl;
+			g_run = 1; cout << "Single step, run = " << g_run << endl;
 		}
 		if (c == 'r') // run mode
 		{
-			g_run = -1;
-			cout << "Run mode, run = " << g_run << endl;
+			g_run = -1; cout << "Run mode, run = " << g_run << endl;
 		}
 		if (c == 27)
 			break;
@@ -185,3 +202,132 @@ void onTrackbarSlide(int pos, void *) {
 		g_run = 1;
 	g_dontset = 0;
 }
+
+// simple transformation of image
+int image_transform_gausian_blur() {
+	Mat image = imread(image_file_path);
+
+	// Create some windows to show the input
+	// and output images in.
+	//
+	cv::namedWindow("Image in", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Gaussian Blur", cv::WINDOW_AUTOSIZE);
+	// Create a window to show our input image
+	//
+	cv::imshow("Image in", image);
+	// Create an image to hold the smoothed output
+	cv::Mat out;
+	// Do the smoothing
+	// Could use GaussianBlur(), blur(), medianBlur() or bilateralFilter().
+	cv::GaussianBlur(image, out, cv::Size(5, 5), 3, 3);
+	cv::GaussianBlur(out, out, cv::Size(5, 5), 3, 3);
+	// Show the smoothed image in the output window
+	//
+	cv::imshow("Gaussian Blur", out);
+	// Wait for the user to hit a key, windows will self destruct
+	//
+	cv::waitKey(0);
+	return(0);
+}
+
+// simple transformation of image
+int image_transform_blur() {
+	Mat image = imread(image_file_path);
+
+	// Create some windows to show the input
+	// and output images in.
+	//
+	cv::namedWindow("Image in", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Blur", cv::WINDOW_AUTOSIZE);
+	// Create a window to show our input image
+	//
+	cv::imshow("Image in", image);
+	// Create an image to hold the smoothed output
+	cv::Mat out;
+	// Do the smoothing
+	// Could use GaussianBlur(), blur(), medianBlur() or bilateralFilter().
+	cv::blur(image, out, cv::Size(5, 5));
+	cv::blur(out, out, cv::Size(5, 5));
+	// Show the smoothed image in the output window
+	//
+	cv::imshow("Blur", out);
+	// Wait for the user to hit a key, windows will self destruct
+	//
+	cv::waitKey(0);
+	return(0);
+}
+
+// simple transformation of image
+int image_transform_median_blur() {
+	Mat image = imread(image_file_path);
+
+	// Create some windows to show the input
+	// and output images in.
+	//
+	cv::namedWindow("Image in", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Median Blur", cv::WINDOW_AUTOSIZE);
+	// Create a window to show our input image
+	//
+	cv::imshow("Image in", image);
+	// Create an image to hold the smoothed output
+	cv::Mat out;
+	// Do the smoothing
+	// Could use GaussianBlur(), blur(), medianBlur() or bilateralFilter().
+	cv::medianBlur(image, out, 5);
+	cv::medianBlur(out, out, 5);
+	// Show the smoothed image in the output window
+	//
+	cv::imshow("Median Blur", out);
+	// Wait for the user to hit a key, windows will self destruct
+	//
+	cv::waitKey(0);
+	return(0);
+}
+
+// simple trasformation of an image
+int image_transform_bilateral_filter() {
+	Mat image = imread(image_file_path);
+
+	// Create some windows to show the input
+	// and output images in.
+	//
+	cv::namedWindow("Image in", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Bilateral Filter", cv::WINDOW_AUTOSIZE);
+	// Create a window to show our input image
+	//
+	cv::imshow("Image in", image);
+	// Create an image to hold the smoothed output
+	cv::Mat out;
+	// Do the smoothing
+	// Could use GaussianBlur(), blur(), medianBlur() or bilateralFilter().
+	cv::bilateralFilter(image, out, 5, 3, 3);
+	cv::bilateralFilter(out, out, 5, 3, 3);
+	// Show the smoothed image in the output window
+	//
+	cv::imshow("Bilateral Filter", out);
+	// Wait for the user to hit a key, windows will self destruct
+	//
+	cv::waitKey(0);
+	return(0);
+}
+
+int complex_transformation() {
+	cv::Mat img = cv::imread(argv[1]), img2;
+	cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Example2", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Example1", img);
+	cv::pyrDown(img, img2);
+	cv::imshow("Example2", img2);
+	cv::waitKey(0);
+	return 0;
+}
+
+int image_transformation_canny_edge() {
+
+}
+
+int camera_input() {
+
+}
+
+
